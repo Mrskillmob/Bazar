8ca447518dc5bc86f22ce5df7849a06273f5a3cd
z19istb12 <z19istb12@z00p3-01.SPSKNM>
2022-03-17 11:46:52 +0100

    skola
    sksk

--- bazar/src/App.js
+++ bazar/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import BazarVyh from './components/BazarVyh'
 import './components/myStyles.css'
+import Footer from './components/Footer'
 
 
 function App() {
-  return (
+  return ( 
     <>
     <BazarVyh  />
-    <div className='text'>Vyhľadávanie</div>
+    <h2 className='textik'>Vyhľadávanie</h2>
     <input type="text" className='vyhladavanie'/>
     <button className='tlacitko'>Hľadaj!</button>
     <input type="text" className='vyhladavanie'/>
     <button className='tlacitko'> Potvrď cenu!</button>
     <input type="text" className='vyhladavanie'/>
     <button className='tlacitko'> Potvrď kategóriu!</button>
-    <button className='boxokolo2'> Test Button!</button>
-    <button className='boxokolo3'> Test Button!</button>
-    <div className='text'>Naše tipy</div>
+     {/*  */}
+    <h1 className='textik'>Naše tipy</h1>
     <img src={require('./images/mys.jpg')} height={300} width={300} className="obrazok" />
     <img src={require('./images/sluchadla.jpg')} height={300} width={300} className="obrazok"/>
     <img src={require('./images/mys.jpg')} height={300} width={300} className="obrazok"/>
@@ -27,14 +27,7 @@ function App() {
     <img src={require('./images/sluchadla.jpg')} height={300} width={300} className="obrazok"/>
     <img src={require('./images/mys.jpg')} height={300} width={300}className="obrazok" />
     <img src={require('./images/sluchadla.jpg')} height={300} width={300} className="obrazok"/>
-    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-      tempor incididunt ut labore et dolore magna aliqua. Vestibulum morbi blandit 
-      cursus risus at ultrices mi tempus. Nisl vel pretium lectus quam. Fames ac 
-      turpis egestas integer eget aliquet nibh. Proin fermentum leo vel orci porta.
-       Etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus.
-        Eget dolor morbi non arcu risus quis varius quam. In est ante in nibh.
-         Etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum.
-          Facilisis mauris sit amet massa. Hendrerit gravida rutrum quisque non.</div>
+          <Footer/>
     </>
   );
 }
--- bazar/src/components/BazarVyh.js
+++ bazar/src/components/BazarVyh.js
@@ -5,6 +5,11 @@ export default function BazarVyh() {
   return (
     <div>
         <h1 class="headerko"> BazarX</h1>
+        <ul>
+            <li> <a>HOME </a>  </li>
+            <li> <a>STORE </a>  </li>
+            <li> <a>TEST </a>  </li>
+        </ul>
     </div>
   )
 }
