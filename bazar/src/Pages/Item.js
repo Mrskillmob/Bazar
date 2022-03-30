f4e303bc9e0b057c3469e8b365221fccf488b1e9
Martin Kubica <71782154+Mrskillmob@users.noreply.github.com>
2022-03-30 23:27:16 +0200

    skusal som to vypisat
    ale neuspesne

--- bazar/src/Pages/Item.js
+++ bazar/src/Pages/Item.js
@@ -1,11 +1,34 @@
-import React from 'react'
+
 import { Link } from 'react-router-dom'
+import { React, useState, useEffect } from "react"
 
 function Item() {
+  
+  const [articles, setArticles] = useState([]);
+useEffect(() => {
+  const fetchData = async () => {
+    const result = await fetch("http://localhost:8080/api/getallitems", {
+      crossDomain: true,
+    });
+    const json = await result.json();
+    //console.log(json);
+    setArticles(json);
+  };
+  fetchData();
+},[articles]);
+  
   return (
     <>
+  
+    
     <h1 className='headerko'>Item</h1>
     <Link to="/home"> <h1 className='textik'>Späť na domovsku stranku</h1></Link>
+    
+    <div>
+      {articles.map((article) => (
+        <Item {...article} />
+      ))}
+      </div>
     
     </>
   )
